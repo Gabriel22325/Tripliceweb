404ba9fb7ba9e7b70a0da14591961a484cf6d87a
Gabriel <gabrielferreira20083007@gmail.com>
2026-05-21 21:37:42 -0300

    configurado para que salve as cores do usuario

--- Atividade01.html.js
+++ Atividade01.html.js
@@ -7,18 +7,26 @@ const corFavorita = document.getElementById("corFavorita");
 corFavorita.addEventListener("input", (e) => {
     let corNova = e.target.value;
 
+    localStorage.setItem("primaryColor", corNova);
+
     document.documentElement.style.setProperty("--primary-color", corNova);
-
-    document.addEventListener(
-        "DOMContentLoaded", () => {
-            const rootSyles =
-                window.getComputedStyle(document.documentElement);
-
-            const primaryColor =
-                rootSyles
-                    .getPropertyValue("--primary-color").trim()
-
-            elementoCor.value = primaryColor;
-
-        });
 });
+
+document.addEventListener(
+    "DOMContentLoaded", () => {
+        if (localStorage.getItem("primaryColor")){
+           corFavorita.value = localStorage.getItem("primaryColor");
+           document.documentElement.style.setProperty("--primary-color", corFavorita.value);
+
+        }
+
+        const rootSyles =
+            window.getComputedStyle(document.documentElement);
+
+        const primaryColor =
+            rootSyles
+                .getPropertyValue("--primary-color").trim()
+
+        elementoCor.value = primaryColor;
+
+    });
